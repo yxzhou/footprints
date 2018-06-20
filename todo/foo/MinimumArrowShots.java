69eb376f36de357c4b739d860d7bb520952a130e
yuanxi <zhouyuanxi@gmail.com>
2018-06-19 17:18:47 -0700

    minor charactor issue

--- todo/foo/MinimumArrowShots.java
+++ todo/foo/MinimumArrowShots.java
@@ -12,7 +12,7 @@ import java.util.Comparator;
  * Start is always smaller than end. There will be at most 10^4 balloons.
  *
  * An arrow can be shot up exactly vertically from different points along the x-axis.
- * A balloon with xstart and xend bursts by an arrow shot at x if xstart ¡Ü x ¡Ü xend.
+ * A balloon with xstart and xend bursts by an arrow shot at x if xstart <<  x << xend.
  * There is no limit to the number of arrows that can be shot. An arrow once shot keeps travelling up infinitely.
  * The problem is to find the minimum number of arrows that must be shot to burst all balloons.
  *
