244aa5131038c868d679804f44b2242319f1713f
joeyz <joeyz@vmware.com>
2018-11-05 12:29:04 -0800

    dailyCoding: SudokuSolver and TinyURL

---
+++ dailyCoding/SudokuSolver.java
@@ -0,0 +1,75 @@
+package fgafa.dailyCoding;
+
+/**
+ *
+ * Sudoku is a puzzle where you're given a partially-filled 9 by 9 grid with digits. The objective is to fill the grid with the constraint that every row, column, and box (3 by 3 subgrid) must contain all of the digits from 1 to 9.
+ * Implement an efficient sudoku solver.
+ *
+ *  Tags: dropbox
+ */
+
+public class SudokuSolver {
+    final static int size = 9;
+
+    public boolean solver(int[][] sudoku){
+        boolean[][] rows = new boolean[size][size];
+        boolean[][] columns = new boolean[size][size];
+        boolean[][] squares = new boolean[size][size];
+
+        for(int row = 0; row < size; row++){
+            for(int column = 0; column < size; column++){
+                int value = sudoku[row][column];
+                if( value != 0 ){
+                    int squareId = (row % 3) * 3 + (column % 3);
+                    if(rows[row][value] || columns[column][value] || squares[squareId][value] ){
+                        return false;
+                    }
+
+                    rows[row][value] = true;
+                    columns[column][value] = true;
+                    squares[squareId][value] = true;
+                }
+            }
+        }
+
+        return helper(sudoku, rows, columns, squares, 0, 0);
+    }
+
+    private boolean helper(int[][] sudoku, boolean[][] rows, boolean[][] columns, boolean[][] squares, int i, int j){
+        if(j == 9){
+            i++;
+            j = 0;
+        }
+
+        for(int row = i; row < size; row++) {
+            for (int column = j; column < size; column++) {
+                if (sudoku[row][column] == 0) {
+                    int squareId = (row % 3) * 3 + (column % 3);
+
+                    for (int value = 0; value < size; value++) {
+                        if (!rows[row][value] && !columns[column][value] && !squares[squareId][value]) {
+
+                            sudoku[row][column] = value;
+                            rows[row][value] = true;
+                            columns[column][value] = true;
+                            squares[squareId][value] = true;
+
+                            if (helper(sudoku, rows, columns, squares, row, column + 1)) {
+                                return true;
+                            }
+
+                            sudoku[row][column] = 0;
+                            rows[row][value] = false;
+                            columns[column][value] = false;
+                            squares[squareId][value] = false;
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ dailyCoding/TinyURL.java
@@ -0,0 +1,65 @@
+package fgafa.dailyCoding;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ *
+ * Implement a URL shortener with the following methods:
+ * shorten(url), which shortens the url into a six-character alphanumeric string, such as zLg6wl.
+ * restore(short), which expands the shortened string into the original url. If no such shortened string exists, return null.
+ *
+ * Hint: What if we enter the same URL twice?
+ *
+ */
+
+public class TinyURL {
+    final static int size = 62;
+    static char[] chars = new char[size];
+    static{
+        int i = 0;
+        for(char c = '0'; c <= '9'; c++){
+            chars[i++] = c;
+        }
+        for(char c = 'a'; c <= 'z'; c++){
+            chars[i++] = c;
+        }
+        for(char c = 'A'; c <= 'Z'; c++){
+            chars[i++] = c;
+        }
+    }
+
+    static volatile AtomicLong count = new AtomicLong();
+
+    static volatile ConcurrentHashMap<String, String> tiny2Long = new ConcurrentHashMap<>();
+    static volatile ConcurrentHashMap<String, String> long2Tiny = new ConcurrentHashMap<>();
+
+    public String shorten(String url){
+        if(long2Tiny.containsKey(url)){
+            return long2Tiny.get(url);
+        }
+
+        long id = count.getAndIncrement();
+
+        StringBuilder result = new StringBuilder();
+        while(id > 0){
+            result.append(chars[(int)(id % size)]);
+            id /= size;
+        }
+
+        String tinyURL = result.toString();
+
+        tiny2Long.put(tinyURL, url);
+        long2Tiny.put(url, tinyURL);
+
+        return tinyURL;
+    }
+
+    public String restore(String tinyURL){
+        if(tiny2Long.containsKey(tinyURL)){
+            return tiny2Long.get(tinyURL);
+        }
+
+        throw new IllegalArgumentException("The tinyURL was not shorten before.");
+    }
+}
