2179d0434c74f071461a368586b2112e18e2399b
joeyz <joeyz@vmware.com>
2018-12-13 12:52:36 -0800

    dailyCoding: graph/Course

---
+++ dailyCoding/LocationRules_todo.java
@@ -0,0 +1,57 @@
+package fgafa.dailyCoding;
+
+import java.util.Dictionary;
+
+/**
+ *
+ * A rule looks like this:
+
+ A NE B
+ This means this means point A is located northeast of point B.
+
+ A SW C
+ means that point A is southwest of C.
+
+ Given a list of rules, check if the sum of the rules validate.
+
+ For example:
+1)  A N B
+    B NE C
+    C N A
+    does not validate, since A cannot be both north and south of C.
+
+2)  A NW B
+    A N B
+    is considered valid.
+
+ *
+ */
+
+public class LocationRules_todo {
+
+    enum Direction{
+        N, S, W, E;
+    }
+
+    class Rule{
+        String from;
+        Direction d1;
+        Direction d2;
+        String to;
+    }
+
+    class Location<String>{
+        String point;
+
+    }
+
+    public boolean validRules(Rule[] rules){
+        if(null == rules || rules.length < 2){
+            return false;
+        }
+
+        //todo
+        return false;
+    }
+
+}
---
+++ dailyCoding/graph/Courses.java
@@ -0,0 +1,93 @@
+package fgafa.dailyCoding.graph;
+
+import java.util.*;
+
+import fgafa.util.Misc;
+import org.junit.Test;
+
+/**
+ *
+ * We're given a hashmap with a key courseId and value a list of courseIds, which represents that the prerequsite of courseId is courseIds. Return a sorted ordering of courses such that we can finish all courses.
+
+ Return null if there is no such ordering.
+
+ For example, given {'CSC300': ['CSC100', 'CSC200'], 'CSC200': ['CSC100'], 'CSC100': []}, should return ['CSC100', 'CSC200', 'CSCS300'].
+ *
+ * Tags: airbnb
+ *
+ */
+
+public class Courses {
+
+    public List<String> courses(Map<String, List<String>> prerequsites){
+
+        if(prerequsites == null || 0 == prerequsites.size()){
+            return null;
+        }
+
+        int size = prerequsites.size();
+        String[] courseId2Names = new String[size];
+        Map<String, Integer> courseName2Ids = new HashMap<>(size);
+        int id = 0;
+        for(String courseName : prerequsites.keySet()) {
+            courseId2Names[id] = courseName;
+            courseName2Ids.put(courseName, id);
+            id++;
+        }
+
+        int[] indegrees = new int[size];
+        Map<Integer, List<Integer>> nexts = new HashMap<>(size);
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(String courseName : prerequsites.keySet()){
+            int childId = courseName2Ids.get(courseName);
+            indegrees[childId] = prerequsites.get(courseName).size();
+
+            if(indegrees[childId] == 0){
+                queue.add(childId);
+            }
+
+            for(String prerequsite : prerequsites.get(courseName)){
+                int parentId = courseName2Ids.get(prerequsite);
+                if(!nexts.containsKey(parentId)){
+                    nexts.put(parentId, new ArrayList<>());
+                }
+
+                nexts.get(parentId).add(childId);
+            }
+        }
+
+        List<String> sequence = new ArrayList<>(size);
+
+        while(!queue.isEmpty()){
+            int courseId = queue.poll();
+            sequence.add(courseId2Names[courseId]);
+
+            if(!nexts.containsKey(courseId)){
+                continue;
+            }
+
+            for(Integer childId : nexts.get(courseId)){
+                if(indegrees[childId] == 1){
+                    queue.add(childId);
+                }else{
+                    indegrees[childId]--;
+                }
+            }
+        }
+
+        return size == sequence.size()? sequence : null;
+    }
+
+    @Test public void test(){
+
+        Map<String, List<String>> prerequsites = new HashMap<>();
+        prerequsites.put("CSC300", Arrays.asList(new String[]{"CSC100", "CSC200"}));
+        prerequsites.put("CSC200", Arrays.asList(new String[]{"CSC100"}));
+        prerequsites.put("CSC100", Arrays.asList(new String[]{}));
+
+        Misc.printList(courses(prerequsites));
+
+    }
+}
